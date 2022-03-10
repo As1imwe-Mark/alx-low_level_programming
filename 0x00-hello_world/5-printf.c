da6feebeb9403da989a3fb2c170ab204a3a6e928
As1imwe-Mark <100065970+As1imwe-Mark@users.noreply.github.com>
2022-03-11 00:00:44 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,8 @@
 *
 *Return: Always 0 (success)
 */
-int main (void)
+int main(void)
 {
-         printf("with proper grammer, but the outcome is apiece of art,");
-         return(0);
+         printf("with proper grammer, but the outcome is a piece of art,");
+         return (0);
 }
