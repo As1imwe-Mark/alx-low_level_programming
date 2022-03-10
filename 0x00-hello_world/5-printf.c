6e02714ced5915a17a90e2ca130569e344a4dce6
As1imwe-Mark <100065970+As1imwe-Mark@users.noreply.github.com>
2022-03-11 00:20:14 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,18 +2,12 @@
 
 /**
 *
-*main - Entry point
-*
-*Description: Prints "with proper grammar, but the outcome is a piece of art,"
-*
-*Return: Always 0 (success)
+*main - Prints the phrase with proper grammar,
+*but the outcome is a piece of art,
+*Return: 0 (success)
 */
 int main (void)
-
 {
-         
-         printf ("with proper grammar, but the outcome is a piece of art,");
-        
-         return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
