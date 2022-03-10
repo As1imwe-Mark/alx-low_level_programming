052f56eaef72e35500c3554bb7ab1f888027eabe
As1imwe-Mark <100065970+As1imwe-Mark@users.noreply.github.com>
2022-03-11 00:37:18 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
 *Return: 0 if properly, non-zero otherwise
 */
 int main(void)
+}
 printf("size of a char : %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n",sizeof(long int));
